f69cab506d126083df387fa0372729c32debefc0
Kisna28 <kisnapatel9909@gmail.com>
2023-11-17 14:54:15 +0530

    More data are add

--- app/src/main/java/com/example/digitaldukan/detailAct.kt
+++ app/src/main/java/com/example/digitaldukan/detailAct.kt
@@ -1,32 +1,46 @@
 package com.example.digitaldukan
 
-import android.content.Intent
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.RatingBar
 import android.widget.TextView
-import android.widget.Toast
 import com.squareup.picasso.Picasso
 
 class detailAct : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
-        val imagedescs: ImageView = findViewById(R.id.imagedesc)
-        val textdesc: TextView = findViewById(R.id.textdesc)
+
+        val imagedescs: ImageView = findViewById(R.id.imageViewThumbnail)
+        val textViewTitle: TextView = findViewById(R.id.textViewTitle)
+        val textViewBrand: TextView = findViewById(R.id.textViewBrand)
+        val textViewCategory: TextView = findViewById(R.id.textViewCategory)
+        val textViewDescription: TextView = findViewById(R.id.textViewDescription)
+        val textViewPrice: TextView = findViewById(R.id.textViewPrice)
+        val textViewDiscountPercentage: TextView = findViewById(R.id.textViewDiscountPercentage)
+        val ratingBar: RatingBar = findViewById(R.id.ratingBar)
+        val textViewStock: TextView = findViewById(R.id.textViewStock)
+
         val selectedProduct = intent.getSerializableExtra("selectedProduct") as Product
 
         // Use the selectedProduct data to update your UI or perform other actions
         // For example, you can load the image into an ImageView:
 
         Picasso.get().load(selectedProduct.thumbnail).into(imagedescs)
-        textdesc.text = selectedProduct.title
+        textViewTitle.text = selectedProduct.title
+        textViewBrand.text = selectedProduct.brand
+        textViewCategory.text = selectedProduct.category
+        textViewDescription.text = selectedProduct.description
+        textViewPrice.text = "Price :$${selectedProduct.price}"
+        textViewDiscountPercentage.text = "Discount :${selectedProduct.discountPercentage}%"
+        ratingBar.rating = selectedProduct.rating.toFloat()
+        textViewStock.text = "In Stock :${selectedProduct.stock}"
 
 
-        /*
-           val model = intent.getParcelableExtra<Product>("image")
-                textdesc.text = model?.title
-        */
+
 
 
     }
